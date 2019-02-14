3866a4f068eff1652e87b46f5ad026cd4b976715
jacob sanders <jkehsanders@gmail.com>
2019-02-14 15:06:46 -0600

    it never ends

--- src/lib/grunt/bgShell.js
+++ src/lib/grunt/bgShell.js
@@ -1,6 +1,6 @@
 module.exports = {
   launchAPI: {
-      cmd: "json-server -p 3002 -w ../../api/database.json"
+      cmd: "json-server -p 3005 -w ../../api/database.json"
   },
   _defaults: {
       bg: true
