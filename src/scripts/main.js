4cd1fb3c2cfadc615655222c8bbda0e28d46bd45
jacob sanders <jkehsanders@gmail.com>
2019-02-21 11:01:36 -0600

    css is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,6 @@
 import welcomePage from "./welcomePage";
 import registrationEventListener from "./registrationEventlistener";
 import mainPage from "./mainPage";
-import chatSection from "./chats/chatsHTML";
-import CreateChatDOM from "./chats/chatsToDOM";
-import addNewsArticle from "./newsArticles/newsArticleToHTML";
-import tasksPage from "./Tasks/task";
-import eventListHTML from "./eventScripts/eventListHTML";
 
 const getfromsessionstorage = sessionStorage.getItem("userId")
 console.log(getfromsessionstorage)
--- src/scripts/mainPage.js
+++ src/scripts/mainPage.js
@@ -1,25 +1,16 @@
 import chatSection from "./chats/chatsHTML";
 import CreateChatDOM from "./chats/chatsToDOM";
-
-import eventListHTML from "./eventScripts/eventListHTML"
-
 import addNewsArticle from "./newsArticles/newsArticleToHTML";
-
 import tasksPage from "./Tasks/task"
-
-
+import eventListHTML from "./eventScripts/eventListHTML"
 const mainPage = () => {
-
-    // document.querySelector("#output").innerHTML += ""
 
     // document.querySelector("#output").innerHTML += ""
     chatSection()
     addNewsArticle()
     CreateChatDOM()
-    tasksPage()
     eventListHTML()
+    tasksPage()
 }
 
 export default mainPage
-
-
