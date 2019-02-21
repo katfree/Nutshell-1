4392a93e76ba812f2052b40cd14730ed438ff384
ANTONIO JEFFERSON <akjeff025@gmail.com>
2019-02-21 09:23:52 -0600

    other

--- src/scripts/mainPage.js
+++ src/scripts/mainPage.js
@@ -1,22 +1,16 @@
 import chatSection from "./chats/chatsHTML";
 import CreateChatDOM from "./chats/chatsToDOM";
-<<<<<<< HEAD
 import eventListHTML from "./eventScripts/eventListHTML"
 
 const mainPage = () => {
 
     document.querySelector("#output").innerHTML += ""
-    eventListHTML()
-=======
-import tasksPage from "./Tasks/task"
-
-const mainPage = () => {
 
     // document.querySelector("#output").innerHTML += ""
->>>>>>> master
     chatSection()
     CreateChatDOM()
     tasksPage()
+    eventListHTML()
 }
 
 export default mainPage
